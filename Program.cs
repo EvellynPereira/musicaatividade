d6a9ba13b71281e9dd7b15de2af7fb9d82850b44
evellyn lacerda <evellynl464@gmail.com>
2022-09-04 22:08:36 -0300

    resolução da atividade musica

---
+++ Program.cs
@@ -0,0 +1,3 @@
+﻿Console.WriteLine("E que se dane a minha postura Se eu mudei, você não viu");
+Console.WriteLine("Eu só queria ter você por perto Mas você sumiu");
+Console.WriteLine("Canção De Quem É A Culpa? da Marília Mendonça");
